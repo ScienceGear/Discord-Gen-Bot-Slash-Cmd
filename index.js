0db050312cb4280f9d6fa5adb54e99f128bcb6fe
ScienceGear <86247572+ScienceGear@users.noreply.github.com>
2022-12-17 16:45:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const { Routes } = require("discord-api-types/v9");
 const { clientId, guildId, token } = require("./config.json");
 const fs = require("fs");
 const generated = new Set();
+const express = require('express')
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Yo boi!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
